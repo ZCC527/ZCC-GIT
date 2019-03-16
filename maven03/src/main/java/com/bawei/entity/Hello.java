7ab469a8a2f05637cbe35f2b7e0701e462607e66
ZCC527 <1749431331@qq.com>
2019-03-16 09:40:37 +0800

    123

---
+++ maven03/src/main/java/com/bawei/entity/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Create Date:2019年3月13日
+ */
+package com.bawei.entity;
+
+/**
+ * <br>Title:TODO 类标题
+ * <br>Description:TODO 类功能描述
+ * <br>Author:张冲冲
+ * <br>Date:2019年3月13日
+ */
+public class Hello {
+  public String sayHello() {
+    return "小猪佩奇身上纹，来世还做垃圾人";
+  }
+}
